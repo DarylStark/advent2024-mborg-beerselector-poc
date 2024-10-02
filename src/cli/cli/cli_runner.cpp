82caa4127dc2e625c52bac89a9b2b754ab010171
Daryl Stark <daryl.stark@asml.com>
2024-10-02 08:29:18 +0000

    Made sure a empty prompt is allowed

--- src/cli/cli/cli_runner.cpp
+++ src/cli/cli/cli_runner.cpp
@@ -15,36 +15,41 @@ void CLIRunner::set_prompt(const std::string prompt)
 
 bool CLIRunner::run()
 {
-    std::string command;
-    std::cout << _prompt;
-    std::getline(std::cin, command);
+    while (true)
+    {
+        std::string command;
+        std::cout << _prompt;
+        std::getline(std::cin, command);
 
-    if (std::cin.eof())
-    {
-        // Handle EOF (CTRL+D)
-        std::clearerr(stdin);
-        std::cin.clear();
-        return false;
-    }
-    if (!std::cin.good())
-    {
-        // Handle other input errors
-        std::cin.clear();  // Clear the error state
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    }
+        if (std::cin.eof())
+        {
+            // Handle EOF (CTRL+D)
+            std::clearerr(stdin);
+            std::cin.clear();
+            return false;
+        }
+        if (!std::cin.good())
+        {
+            // Handle other input errors
+            std::cin.clear();  // Clear the error state
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
 
-    std::stringstream iss(command);
-    std::vector<std::string> words(std::istream_iterator<std::string>{iss},
-                                   std::istream_iterator<std::string>());
+        std::stringstream iss(command);
+        std::vector<std::string> words(std::istream_iterator<std::string>{iss},
+                                       std::istream_iterator<std::string>());
 
-    // Parse
-    try
-    {
-        return _parser->parse(words);
-    }
-    catch (const ParseException& e)
-    {
-        std::cerr << e.what() << std::endl;
-        return true;
+        if (words.empty()) continue;
+
+        // Parse
+        try
+        {
+            return _parser->parse(words);
+        }
+        catch (const ParseException& e)
+        {
+            std::cerr << e.what() << std::endl;
+            return true;
+        }
     }
 }
